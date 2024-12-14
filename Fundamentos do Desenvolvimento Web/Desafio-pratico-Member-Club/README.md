0a57d5f0431291be635bdd568c0a8450fe9f3db0
guimouraO1 <guimoura.oliveira@gmail.com>
2024-12-14 17:30:19 -0300

    Desafio prático - Member Club

---
+++ Fundamentos do Desenvolvimento Web/Desafio-pratico-Member-Club/README.md
@@ -0,0 +1,31 @@
+# Desafio Prático - Members Club
+
+## Requisitos Funcionais
+
+1. **Deve ser possível buscar um cliente por ID**: O cliente pode ser buscado digitando o ID e pressionando Enter.
+2. **Deve ser realizada a validação do ID informado**: O sistema verifica se o ID é válido e existe.
+3. **Deve ser exibido um alerta de erro**: Caso o ID informado seja inválido, um alerta de erro deve ser mostrado.
+4. **Deve ser possível exibir os dados do cliente**: Ao encontrar o cliente, a imagem e o nome do cliente devem ser mostrados.
+5. **Deve ser exibido o histórico de cortes do cliente**: O histórico deve mostrar a quantidade de cortes realizados, incluindo a data e hora de cada um.
+6. **Deve ser possível marcar cortes no cartão fidelidade**: Cada corte realizado deve ser marcado no cartão fidelidade do cliente.
+7. **Deve ser exibido o progresso para o prêmio**: O sistema deve mostrar quantos cortes faltam para o prêmio.
+8. **Deve ser exibido um modal de parabéns**: Após 10 cortes, o sistema deve mostrar a mensagem: "Parabéns! Seu próximo corte é gratuito!".
+
+## Ids Disponíveis
+
+-   **Guilherme:** 124-537-835-230
+-   **Diego:** 124-537-835-231
+-   **Mayke:** 124-537-835-232
+
+## Tecnologias Utilizadas
+
+-   HTML
+-   CSS
+-   JavaScript
+-   JSON Server
+
+## Como Usar
+
+1. Clone o projeto para o seu ambiente local.
+2. Instale as dependências com o comando `npm install`.
+3. Abra o arquivo `index.html` no seu navegador.
---
+++ Fundamentos do Desenvolvimento Web/Desafio-pratico-Member-Club/src/js/app.js
@@ -0,0 +1,100 @@
+class ClientProfile {
+	constructor(clientId) {
+		this.clientId = clientId;
+		this.user = null;
+	}
+
+	async fetchUserData() {
+		try {
+			const response = await fetch(`http://localhost:3000/clients/${this.clientId}`);
+			if (!response.ok) throw new Error('ID não encontrado!');
+
+			this.user = await response.json();
+		} catch (error) {
+			alert(error.message);
+			document.getElementById('id').value = '';
+		}
+	}
+
+	renderProfile() {
+		if (!this.user) return;
+
+		document.getElementById('username').textContent = `${this.user.name}`;
+		document.getElementById('clientSince').textContent = `Cliente desde ${this.user.clientSince}`;
+		document.getElementById('profile-pic-id').innerHTML = `<img src="${this.user.profilePic}" alt="Profile Image">`;
+		document.getElementById('history-header').innerHTML = `<p>Histórico</p> <p>${this.user.loyaltyCard.totalCuts} cortes</p>`;
+		document.getElementById('card-header-id-span').textContent = `ID: ${this.user.id}`;
+		this.renderAppointmentHistory();
+		this.renderLoyaltyCard();
+		this.renderProgressBar();
+	}
+
+	renderAppointmentHistory() {
+		const cutsContainer = document.getElementById('cuts');
+		cutsContainer.innerHTML = '';
+		this.user.appointmentHistory.forEach((cut) => {
+			cutsContainer.innerHTML += `
+          <div class="corte">
+            <div class="corte-date">
+              <p>${cut.date}</p>
+              <p>${cut.time}</p>
+            </div>
+            <div class="corte-icon">
+              <i class="ph ph-seal-check"></i>
+            </div>
+          </div>`;
+		});
+	}
+
+	renderLoyaltyCard() {
+		const cardContent = document.getElementById('card-content');
+		cardContent.innerHTML = '';
+
+		for (let i = 0; i < (this.user.loyaltyCard.totalCuts >= this.user.loyaltyCard.cutsNeeded ? this.user.loyaltyCard.totalCuts - 1 : this.user.loyaltyCard.totalCuts); i++) {
+			cardContent.innerHTML += `<div class="card-content-corte"><img src="./assets/PinCheck.png" alt="Pin-Check-image"></div>`;
+		}
+
+		for (let i = 0; i < this.user.loyaltyCard.cutsRemaining - 1; i++) {
+			cardContent.innerHTML += `<div class="card-content-corte"></div>`;
+		}
+
+		if (this.user.loyaltyCard.totalCuts === 10) {
+			cardContent.innerHTML += `<div class="card-content-corte"><img src="./assets/PinGift.png" alt="Gift-image"></div>`;
+		} else {
+			cardContent.innerHTML += `<div class="card-content-corte"><i class="ph-fill ph-gift"></i></div>`;
+		}
+	}
+
+	renderProgressBar() {
+		const progressItems = document.getElementById('progress-items');
+		progressItems.innerHTML = `
+        <span>${this.user.loyaltyCard.cutsRemaining} cortes restantes</span>
+        <div class="progress-footer">
+          <div class="progress-bar">
+            <div class="total-progress" style="width: ${this.user.loyaltyCard.totalCuts * 10}%;"></div>
+          </div>
+          <span>${this.user.loyaltyCard.totalCuts} de ${this.user.loyaltyCard.cutsNeeded}</span>
+        </div>`;
+	}
+}
+
+async function loadClientProfile(clientId) {
+	const clientProfile = new ClientProfile(clientId);
+	await clientProfile.fetchUserData();
+	clientProfile.renderProfile();
+}
+window.addEventListener('load', () => loadClientProfile('124-537-835-230'));
+
+document.getElementById('id').addEventListener('keypress', function (event) {
+	if (event.key === 'Enter') {
+		event.preventDefault();
+		searchUserById();
+	}
+});
+
+async function searchUserById() {
+	const id = document.getElementById('id').value;
+	if (id) {
+		await loadClientProfile(id);
+	}
+}
